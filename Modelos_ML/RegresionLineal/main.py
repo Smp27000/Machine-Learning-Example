ea44406493867467c7dc7816011bd0b4506597d0
Smp27000 <samuelpalaciohoyos@gmail.com>
2026-09-10 12:32:56 -0500

    Cambios en el main para el buen funcionamiento de la api

--- Modelos_ML/RegresionLineal/main.py
+++ Modelos_ML/RegresionLineal/main.py
@@ -1,3 +1,41 @@
 import joblib
 from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel, Field
+
+app = FastAPI(
+    title='API de predicción de precio',
+    description='API para predecir el precio de una casa segun su tamaño en m2',
+    version='1.0.0'
+)
+
+try:
+    #Cargar el modelo entrenado
+    model = joblib.load('Modelos_ML/RegresionLineal/Models/linear_regression_model.pkl')
+
+except Exception:
+    model = None
+
+class House_m2(BaseModel):
+    area_m2 : float = Field(..., example = 82.5, description="Area de la casa en m2", gt=0)
+
+@app.get("/")
+def health_check():
+    return {
+        "message": "API de prediccion de precios de viviendas en funcionamiento",
+        "Status" : "OK",
+        "model" : "Regresion Lineal",
+        "version" : "1.0.0",
+        "model_loaded" : model is not None
+    }
+
+@app.post("/predict")
+def prdict_price_m2(data:House_m2): 
+    if not model:
+        raise HTTPException(status_code=500, detail="Model not found or not loaded")
+    
+    prediction = model.predict([[data.area_m2]])[0]
+    return {
+        "area_m2" : data.area_m2,
+        "predicted_price" : round(prediction, 2)
+    }
+    
--- Modelos_ML/RegresionLineal/train.py
+++ Modelos_ML/RegresionLineal/train.py
@@ -1,6 +1,7 @@
 import joblib
 import numpy as np
 from sklearn.linear_model import LinearRegression
+import matplotlib.pyplot as plt
 
 #Datos de entrenamiento (X) y etiquetas (y)
 x = np.array([[50], [40], [70], [100], [60]])
@@ -11,5 +12,27 @@ y = np.array([6000000, 4500000, 8200000, 11000000, 7000000])
 model = LinearRegression()
 model.fit(x, y)
 
+# #Predicciones de prueba
+# y_pred = model.predict(x)
+
+# #Impremir los resultados de la informacion del modelo
+# print("Coeficiente de regresion:", model.coef_[0])
+# print("Interseccion:", model.intercept_)
+
+# #Graficar los datos de entrenamiento
+# plt.scatter(x, y, color='blue', label='Datos de entrenamiento')
+
+# #Graficar la linea de regresion
+# plt.plot(x, y_pred, color='red', label='Lineal de regresion')
+# plt.xlabel('Superficie (m2)')
+# plt.ylabel('Precio (COP)')
+# plt.title('Regresion Lineal: Precio de vivienda segun su superficie')
+# plt.legend()
+# plt.grid(True)
+
+# #Imprimir la grafica
+# plt.show()
+
 #Guardar el artefacto del modelo entrenado en un archivo
-joblib.dump()
+joblib.dump(model, 'Modelos_ML/RegresionLineal/Models/linear_regression_model.pkl')
+
